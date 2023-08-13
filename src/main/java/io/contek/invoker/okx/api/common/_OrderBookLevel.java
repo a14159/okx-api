c811dd1e5fbc7194c47f5bc1adc2948a6db64772
kong <kong_k@hotmail.com>
2023-08-13 10:09:13 +0300

    _OrderBook and friends: types for fields

--- src/main/java/io/contek/invoker/okx/api/common/_OrderBookLevel.java
+++ src/main/java/io/contek/invoker/okx/api/common/_OrderBookLevel.java
@@ -1,7 +1,30 @@
 package io.contek.invoker.okx.api.common;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 @NotThreadSafe
-public class _OrderBookLevel extends ArrayList<String> {}
+public class _OrderBookLevel extends ArrayList<String> {
+
+    private BigDecimal price = null;
+    private BigDecimal qty = null;
+
+    public BigDecimal getPrice() {
+        if (price != null)
+            return price;
+        price = new BigDecimal(get(0));
+        return price;
+    }
+
+    public BigDecimal getSize() {
+        if (qty != null)
+            return qty;
+        qty = new BigDecimal(get(1));
+        return qty;
+    }
+
+    public int getOrdersAtLevel() {
+        return Integer.parseInt(get(3));
+    }
+}

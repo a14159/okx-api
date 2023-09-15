0aba34eb29d7a4f816320019ddfad8439f8abdd3
kong <kong_k@hotmail.com>
2023-09-15 22:05:23 +0300

    Balance fixes

--- src/main/java/io/contek/invoker/okx/api/common/_AccountBalance.java
+++ src/main/java/io/contek/invoker/okx/api/common/_AccountBalance.java
@@ -7,12 +7,12 @@ import java.util.List;
 @NotThreadSafe
 public class _AccountBalance {
 
-  public BigDecimal adjEq; // Adjusted / Effective equity in USD
-  public BigDecimal imr; // Initial margin requirement in USD
+  public String adjEq; // Adjusted / Effective equity in USD
+  public String imr; // Initial margin requirement in USD
   public BigDecimal isoEq; // Isolated margin equity in USD
   public String mgnRatio; // Margin ratio in USD
-  public BigDecimal mmr; // Maintenance margin requirement in USD
-  public BigDecimal notionalUsd; // Notional value of positions in USD
+  public String mmr; // Maintenance margin requirement in USD
+  public String notionalUsd; // Notional value of positions in USD
   public String ordFroz; // Cross margin frozen for pending orders in USD
   public BigDecimal totalEq; // The total amount of equity in USD
   public long uTime; // Update time of account information, millisecond format of Unix timestamp, e.g. 1597026383085
--- src/main/java/io/contek/invoker/okx/api/common/_EquityBalance.java
+++ src/main/java/io/contek/invoker/okx/api/common/_EquityBalance.java
@@ -1,5 +1,7 @@
 package io.contek.invoker.okx.api.common;
 
+import io.contek.invoker.okx.api.Util;
+
 import javax.annotation.concurrent.NotThreadSafe;
 import java.math.BigDecimal;
 
@@ -8,26 +10,31 @@ public class _EquityBalance {
 
   public BigDecimal availBal; // Available balance of the currency
   public BigDecimal availEq; // Available equity of the currency
+  public String borrowFroz;
   public BigDecimal cashBal; // Cash balance
   public String ccy; // Currency
-  public BigDecimal crossLiab; // Cross liabilities of the currency
+  public String crossLiab; // Cross liabilities of the currency
   public BigDecimal disEq; // Discount equity of the currency in USD.
   public BigDecimal eq; // Equity of the currency
   public BigDecimal eqUsd; // Equity in USD of the currency
   public BigDecimal frozenBal; // Frozen balance of the currency
-  public BigDecimal interest; // Accrued interest of the currency
+  public String interest; // Accrued interest of the currency
   public BigDecimal isoEq; // Isolated margin equity of the currency
-  public BigDecimal isoLiab; // Isolated liabilities of the currency
+  public String isoLiab; // Isolated liabilities of the currency
   public BigDecimal isoUpl; // Isolated unrealized profit and loss of the currency
-  public BigDecimal liab; // Liabilities of the currency
-  public BigDecimal maxLoan; // Max loan of the currency
+  public String liab; // Liabilities of the currency
+  public String maxLoan; // Max loan of the currency
   public String mgnRatio; // Margin ratio of the currency
   public String notionalLever; // Leverage of the currency Applicable to Single-currency margin
   public BigDecimal ordFrozen; // Margin frozen for open orders
   public BigDecimal twap; // Risk indicator of auto liability repayment Divided into 5 levels, from 1 to 5, the larger the number, the more likely the auto repayment will be triggered.
   public long uTime;
   public BigDecimal upl; // The sum of the unrealized profit & loss of all margin and derivatives positions of the currency.
-  public BigDecimal uplLiab; // Liabilities due to Unrealized loss of the currency
+  public String uplLiab; // Liabilities due to Unrealized loss of the currency
   public BigDecimal stgyEq; // Strategy equity
-  public BigDecimal spotInUseAmt; // Spot in use amount
+  public String spotInUseAmt; // Spot in use amount
+
+  public BigDecimal getLiab() {
+    return Util.parseString(liab, BigDecimal.ZERO);
+  }
 }

336f577fa99d4f659c4de2d5ae3492fbf38a08b1
kong <kong_k@hotmail.com>
2023-09-18 15:37:16 +0300

    Balance fixes

--- src/main/java/io/contek/invoker/okx/api/common/_Position.java
+++ src/main/java/io/contek/invoker/okx/api/common/_Position.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public class _Position {
 
   public String adl;
-  public BigDecimal availPos;
+  public String availPos;
   public BigDecimal avgPx;
   public String cTime;
   public String ccy;

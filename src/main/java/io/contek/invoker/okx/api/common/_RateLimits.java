698f6401d780164073ad0cf6858c485a37f279af
kong <kong_k@hotmail.com>
2024-09-23 15:06:41 +0300

    GetAccountRateLimits impl - bugfix parse BigDecimal

--- src/main/java/io/contek/invoker/okx/api/common/_RateLimits.java
+++ src/main/java/io/contek/invoker/okx/api/common/_RateLimits.java
@@ -3,12 +3,22 @@ package io.contek.invoker.okx.api.common;
 import javax.annotation.concurrent.NotThreadSafe;
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ZERO;
+
 @NotThreadSafe
 public class _RateLimits {
 
-    public BigDecimal fillRatio;
-    public BigDecimal mainFillRatio;
+    public String fillRatio;
+    public String mainFillRatio;
     public Integer accRateLimit;
     public Integer nextAccRateLimit;
     public long ts;
+
+    public BigDecimal getFillRatio() {
+        return Util.parseString(fillRatio, ZERO);
+    }
+
+    public BigDecimal getMainFillRatio() {
+        return Util.parseString(mainFillRatio, ZERO);
+    }
 }
